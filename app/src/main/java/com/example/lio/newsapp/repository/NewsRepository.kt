af160672c59ae9f2fccc5d539719a2be5c5b627c
Lio <liomazza@gmail.com>
2020-07-15 11:13:20 -0300

    get breaking news api response

--- app/src/main/java/com/example/lio/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/example/lio/newsapp/repository/NewsRepository.kt
@@ -1,9 +1,12 @@
 package com.example.lio.newsapp.repository
 
+import com.example.lio.newsapp.api.RetrofitClient
 import com.example.lio.newsapp.db.ArticleDatabase
 
 class NewsRepository (
     val db: ArticleDatabase
 ){
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitClient.api.getBreakingNews(countryCode, pageNumber)
 
 }
